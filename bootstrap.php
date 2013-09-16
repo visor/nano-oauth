2f9b7ee954e910d4af5cbc8412017fe85e1405b8
Victor Karamzin <admin@visor.ws>
2013-09-16 18:51:12 +0700

    Initial commit

---
+++ bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+error_reporting(E_ALL | E_STRICT);
+ini_set('error_log', __DIR__ . '/build/logs/error.log');
+ini_set('display_errors', true);
+
+if (!class_exists('Nano\Application', false)) {
+	include __DIR__ . '/dependencies/nano/core/library/Application.php';
+}
+
+$application = \Nano\Application::create()
+	->withConfigurationFormat('php')
+	->withRootDir(__DIR__)
+	->withModule('oauth', __DIR__)
+	->configure()
+;
