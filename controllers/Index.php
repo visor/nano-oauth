275e93534a8257d8f12d6c998db110e491f1a830
Victor Karamzin <admin@visor.ws>
2013-09-17 23:20:43 +0700

    refs #1: Page for testing

--- controllers/Index.php
+++ controllers/Index.php
@@ -6,10 +6,17 @@ use Nano\Controller;
 
 class Index extends Controller {
 
+	public $services;
+
 	public function indexAction() {
+		$this->services = app()->oauth->getServices();
 	}
 
 	public function callbackAction() {
+	}
+
+	protected function before() {
+		$this->services = app()->oauth->getServices();
 	}
 
 }
--- views/index/index.php
+++ views/index/index.php
@@ -1,1 +1,5 @@
-<?php
+<?php /** @var \Module\Oauth\Service[] $services */ ?>
+<strong>Login via:</strong><br />
+<?php foreach ($services as $service): ?>
+	<a href="<?php echo $service->getAuthoirizeUrl(\Module\Oauth\Scope::NONE); ?>"><?php echo $service->getName(); ?></a><br />
+<?php endforeach ?>
